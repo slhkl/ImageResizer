e2acbd89cfcaf552797dc3511af4645ac268b6ad
Salih Kol <salih.kol@detaysoft.com>
2024-02-01 10:19:03 +0300

    Add project files.

---
+++ Api/Controllers/ImageResizeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Utils;
+
+namespace Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ImageResizeController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult ResizeImage(IFormFile formFile)
+        {
+            return File(ImageResizer.Instance.ResizedImage(formFile.OpenReadStream()), "image/jpeg");
+        }
+    }
+}
---
+++ Utils/ImageResizer.cs
@@ -0,0 +1,55 @@
+﻿using SkiaSharp;
+
+namespace Utils
+{
+    public class ImageResizer
+    {
+        #region Singleton
+
+        public static ImageResizer Instance => instance.Value;
+
+        private static Lazy<ImageResizer> instance = new Lazy<ImageResizer>(() => new ImageResizer());
+
+        #endregion
+
+        public byte[] ResizedImage(Stream stream, int width = 512)
+        {
+            try
+            {
+                SKBitmap image = SKBitmap.Decode(stream);
+
+                var info = new SKImageInfo(width, (int)(width * (float)image.Height / image.Width));
+                image = image.Resize(info, SKFilterQuality.High);
+
+                using var ms = new MemoryStream();
+                image.Encode(ms, SKEncodedImageFormat.Jpeg, 85);
+                return ms.ToArray();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("ImageResizerEx: " + ex.Message);
+            }
+            return null;
+        }
+
+        public byte[] ResizedImage(byte[] file, int width = 512)
+        {
+            try
+            {
+                SKBitmap image = SKBitmap.Decode(file);
+
+                var info = new SKImageInfo(width, (int)(width * (float)image.Height / image.Width));
+                image = image.Resize(info, SKFilterQuality.High);
+
+                using var ms = new MemoryStream();
+                image.Encode(ms, SKEncodedImageFormat.Jpeg, 85);
+                return ms.ToArray();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("ImageResizerEx: " + ex.Message);
+            }
+            return null;
+        }
+    }
+}
